5dabf180c7ea40a89c02087ce6cf7a6a8c7cd449
David Rojas <david_rojas82142@elpoli.edu.co>
2019-04-24 14:21:49 -0500

    fixes styles sections

--- src/components/Molecules/FollowSection.jsx
+++ src/components/Molecules/FollowSection.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './FollowSection.scss'
+import { ReactComponent as Twitter } from "../../assets/twitter.svg";
+
 const FollowSection = () => (
     <section className="follow">
         <div className="follow__container">
             <h1 className="follow__title"><span>350 + </span> <span>Woloxers</span></h1>
-            <p className="follow__p">@Wolox</p>
+            <p className="follow__p">
+                <Twitter className="follow__p--twitter"/> @Wolox
+            </p>
             <a href="https://www.twitter.com/wolox" className="button--primary__outline">Siguenos</a>
         </div>
         <div className="follow__container">
